2fc6ee2171d2ee1ae9874bf27bc69de201788102
VinceVent <Vchen4070@stu.d214.org>
2023-11-14 10:27:00 -0600

    Initial Commit

---
+++ CPB_FireBase/College Data.swift
@@ -0,0 +1,8 @@
+//
+//  College Data.swift
+//  CPB_FireBase
+//
+//  Created by vincent chen on 11/6/23.
+//
+
+import Foundation
---
+++ CPB_FireBase/College Name.swift
@@ -0,0 +1,8 @@
+//
+//  College Name.swift
+//  CPB_FireBase
+//
+//  Created by vincent chen on 11/6/23.
+//
+
+import Foundation
